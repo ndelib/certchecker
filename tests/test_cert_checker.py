47277a3e10ee77c5482459b9369ad80d265ffeb7
ndelib <69071207+ndelib@users.noreply.github.com>
2020-08-04 22:15:50 +1000

    Started unit test boilerplate

---
+++ tests/test_cert_checker.py
@@ -0,0 +1,19 @@
+from unittest.mock import patch
+from certchecker import CertChecker
+
+class TestCase:
+
+    @classmethod
+    def setup_class(cls):
+        """ TODO """
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        """ TODO """
+        pass
+
+    @patch("certchecker.CertChecker")
+    def test_issuer(self):
+        # TODO
+        pass
