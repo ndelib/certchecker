2f8a4b17b1840e4a1f015d31fe170c0b979abe79
ndelib <69071207+ndelib@users.noreply.github.com>
2020-08-06 00:26:26 +1000

    Added basic mocked unit test

--- tests/test_cert_checker.py
+++ tests/test_cert_checker.py
@@ -1,19 +1,26 @@
 from unittest.mock import patch
-from certchecker import CertChecker
+from certchecker.cert_checker import CertChecker
 
 class TestCase:
 
     @classmethod
     def setup_class(cls):
-        """ TODO """
+        """ TODO implement properly """
         pass
-
+    
     @classmethod
     def teardown_class(cls):
         """ TODO """
         pass
 
-    @patch("certchecker.CertChecker")
-    def test_issuer(self):
-        # TODO
-        pass
+    @patch("certchecker.cert_checker.CertChecker")
+    def test_issuer(self, mocked_checker):
+        parsed_certificate = {
+            "issuer": {"C": "US", "CN": "GTS CA 1O1", "O": "Google Trust Services"}
+        }
+        expected_issuer = {"commonName": "GTS CA 1O1","country": "US",
+            "organisationName": "Google Trust Services"}
+        mocked_checker.return_value.parse_certificate.return_value\
+            = parsed_certificate
+        test_checker = CertChecker("google.com")
+        assert test_checker.issuer == expected_issuer
