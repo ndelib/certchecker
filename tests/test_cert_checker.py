72048ae6b7e396b9c90f98b25947e052db2bc593
ndelib <69071207+ndelib@users.noreply.github.com>
2020-08-08 12:08:40 +1000

    Fix mocked unit test add dummy cert data

--- tests/test_cert_checker.py
+++ tests/test_cert_checker.py
@@ -1,26 +1,37 @@
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 from certchecker.cert_checker import CertChecker
+
+
 
 class TestCase:
 
     @classmethod
     def setup_class(cls):
-        """ TODO implement properly """
+        """ TODO """
         pass
-    
+
     @classmethod
     def teardown_class(cls):
         """ TODO """
         pass
 
-    @patch("certchecker.cert_checker.CertChecker")
-    def test_issuer(self, mocked_checker):
-        parsed_certificate = {
-            "issuer": {"C": "US", "CN": "GTS CA 1O1", "O": "Google Trust Services"}
-        }
+    def test_issuer(self):
         expected_issuer = {"commonName": "GTS CA 1O1","country": "US",
             "organisationName": "Google Trust Services"}
-        mocked_checker.return_value.parse_certificate.return_value\
-            = parsed_certificate
-        test_checker = CertChecker("google.com")
-        assert test_checker.issuer == expected_issuer
+        # Given a dummy Google cert, ensure we extract the correct issuer
+        with patch.object(CertChecker, "_get_certificate", \
+            return_value=self._get_mocked_cert("google.com")):
+            test_checker = CertChecker("google.com")
+            assert test_checker.issuer == expected_issuer
+
+    def _get_mocked_cert(self, host):
+        data_files = {
+            "google.com": "tests/data/dummy_google_cert.pem"
+        }
+        dummy_cert = ""
+        with open(data_files[host], "r") as mock_data:
+            dummy_cert = mock_data.read()
+        return dummy_cert
+
+
+
